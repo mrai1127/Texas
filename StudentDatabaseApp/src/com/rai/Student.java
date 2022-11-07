495ef8377c05e1d9d19f526d4d8cb39a51adca83
Mausam Rai <mausamrai11@gmail.com>
2022-11-06 23:20:40 -0500

    first commit to elpaso

---
+++ StudentDatabaseApp/src/com/rai/Student.java
@@ -0,0 +1,11 @@
+package com.rai;
+
+public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	
+
+}
---
+++ StudentDatabaseApp/src/com/rai/StudentDatabaseApp.java
@@ -0,0 +1,5 @@
+package com.rai;
+
+public class StudentDatabaseApp {
+
+}
